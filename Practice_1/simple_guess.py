63dca9f3789c056c3cbee65fa57205d791ef80a4
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2023-09-07 11:29:04 +0300

    Added the first practice v 1.0

---
+++ Practice_1/simple_guess.py
@@ -0,0 +1,12 @@
+n = int(input("Введите число: \n"))
+
+def guess_root(number: int) -> int | str:
+    for guess in range(number+1):
+        if number == guess**2:
+            return guess
+    return None
+
+print(guess_root(n) if guess_root(n) is not None else "Сложна, не могу")
+
+# ----------------------------------------------------------------------
+# А теперь усложним задачу! С фиксированным шагом и проверкой на эпсилон 
